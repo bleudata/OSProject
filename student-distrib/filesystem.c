e8abb825b3ad154dd3d620030b391caf8b19063d
Kasetphaisansin <kasink2@illinois.edu>
2023-03-24 13:51:02 -0500

    second try filesys

--- student-distrib/filesystem.c
+++ student-distrib/filesystem.c
@@ -1,6 +1,35 @@
-#include "filesys.h"
+#include "filesystem.h"
 
 file_sys_block* boot_block;
 file_sys_block* inode_array;
 file_sys_block* data_array;
 
+//initialize the global variables boot_block, inode_array, data_array
+void file_init(file_sys_block* fileimg_address){
+    boot_block = fileimg_address;
+    inode_array = fileimg_address + 1;
+    data_array = fileimg_address + 1 + boot_block->boot_type.inode_count;
+}
+
+//goes through dir_entries array in bootblock and finds the dir entry with matching name
+int32_t read_dentry_by_name(const uint8_t* fname, d_entry* dentry){
+    //calls read_dentry_by_index
+    int i;
+    for(i = 0; i<63 ; i++){
+        if(strncmp(fname, boot_block->boot_type.dir_entries[i].filename, 4) == 0){ 
+            //filename matches
+            read_dentry_by_index(i, dentry); 
+            break;
+        }
+    }
+}
+
+//read_dentry_by_index
+int32_t read_dentry_by_index(uint32_t index, d_entry* dentry){
+    dentry = &(boot_block->boot_type.dir_entries[index]);
+}
+
+//read_data, offset = no. of bytes offset to start reading from in the file
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
+
+}
--- student-distrib/filesystem.h
+++ student-distrib/filesystem.h
@@ -1,6 +1,8 @@
 #ifndef PAGING_H
 #define PAGING_H
 
+#include "lib.h"
+
 #define FILENAME_LENGTH 4
 
 typedef struct __attribute__ ((packed)){
@@ -16,21 +18,24 @@ typedef struct __attribute__ ((packed)){
     int32_t data_count;
     int8_t reserved[52];
     d_entry dir_entries[63];
-}boot_block;
+}boot_block_struct;
 
 typedef struct __attribute__ ((packed)){
     int32_t length;
     int32_t data_block_num[1023];
-}inode_block;
+}inode_block_struct;
 
 typedef union{
-    boot_block boot_block_type;
-    inode_block inode_type;
+    boot_block_struct boot_type;
+    inode_block_struct inode_type;
 }file_sys_block;
 
-#endif
+
 
 //file_init -> just initialize global variables(pointers to stuff inside the filesystem)
+void filesys_init(file_sys_block* fileimg_address);
+
+
 //fopen (arguments are same as system call) (design choice)
 //fclose
 //fwrite
@@ -42,5 +47,13 @@ typedef union{
 //dread
 
 //read_dentry_by_name
+int32_t read_dentry_by_name(const uint8_t* fname, d_entry* dentry);
+
 //read_dentry_by_index
-//read_data
+int32_t read_dentry_by_index(uint32_t index, d_entry* dentry);
+
+//read_data, offset = no. of bytes offset to start reading from in the file
+int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
+
+
+#endif
